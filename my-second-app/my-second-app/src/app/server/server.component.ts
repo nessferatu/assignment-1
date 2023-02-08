852584c6c436072f8251f6430a2bcf786824dd64
vanessa shogreen <vanessashogreen@vanessas-air.attlocal.net>
2023-02-08 11:54:51 -0500

    Yet another commit.

---
+++ my-second-app/my-second-app/src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector: '[app-server]', //selector is an attribute
+  //selector: '.app-server', //selector is a class
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css'],
+})
+export class ServerComponent {}
